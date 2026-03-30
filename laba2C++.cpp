cb3ef468314dab2af8270b404ddf95a5f94e0d48
ShadowGorila <yamashina000@gmail.com>
2026-03-30 14:01:52 +0500

    Наследование: класс Manufacturer от Pivo, три конструктора, деструктор

--- laba2C++.cpp
+++ laba2C++.cpp
@@ -52,7 +52,49 @@ public:
     }
 };
 
+class Manufacturer : public Pivo {
+private:
+    std::string country;
+public:
+    // Создан без параметров. 
+    Manufacturer() : Pivo () {
+        country = "Unknown";
+        std::cout << "Default Manufacturer created" << std::endl;
+    }
+
+    // Создан с параметрами.
+    Manufacturer(std::string name, double alcoholContent,std::string country) : Pivo(name, alcoholContent) 
+    {
+        this -> country = country;
+        std::cout << "Manufacturer " << name << " from " << country << std::endl;
+    }
+    
+    // Конструктор копирования.
+    Manufacturer(const Manufacturer& other) : Pivo(other) {
+        country = other.country;
+        std::cout << "Manufacturer " << getName() << " copied" << std::endl;
+    }
+
+    // Диструктор.
+    ~Manufacturer() {
+        std:: cout << "Manufacturer copy" << std::endl;
+    }
+
+    void printManufacturer() {
+        std::cout << "Manufacturer: " << getName()
+        << ", Alchol: " << getAlcohol() << "%"
+        << ", Country: " << country << std::endl;
+}
+};
+
 int main() {
+    Manufacturer m1;
+    Manufacturer m2("Bavaria", 5.0, "Germany");
+    Manufacturer m3(m2);
+    m1.printManufacturer();
+    m2.printManufacturer();
+    m3.printManufacturer();
+    std::cout << "------------------------------" << std::endl; 
    Pivo pivo1;
    Pivo pivo2("Bavaria", 7.9);
    Pivo pivo3(pivo2);
